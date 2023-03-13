b7fbf96f97e4d7f3eaf036ac7990826221ba41fc
iwatkot <iwatkot@gmail.com>
2023-03-13 17:49:55 +0300

    Removed Search button, updated README.md.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <a href="https://codeclimate.com/github/iwatkot/search1337bot/maintainability"><img src="https://api.codeclimate.com/v1/badges/4944cffd3dd2510067ad/maintainability" /></a>
 # Overview
-This telegram bot uses [hemantapkh's 1337x](https://github.com/hemantapkh/1337x) unofficial API to search the torrents on 1337x tracker.<br>
-The bot itself built on `aiogram` and has only one command to search the torrents. It shows top ten results for the search query as inline commands. After user pressed one of the results, the bot catches callback data (which is a torrent ID on 1337x) and generates inline button with magnet link. As long Telegram won't let using non-http links, the bot uses `short.io API` to generate shorten URL with a magnet link and then send it to the user.
+This telegram bot uses [hemantapkh's 1337x](https://github.com/hemantapkh/1337x) unofficial API to search the torrents on **1337x** tracker.<br>
+The bot itself is built on **aiogram** and has no commands at all, it expects that every message from the user is a search request. It shows the top ten results for the search query as inline buttons. After the user pressed one of the results, the bot catches callback data (which is a torrent ID on 1337x) and generates an inline button with a magnet link. As long as Telegram does not let the usage of a non-HTTP link, the bot uses `short.io API` to generate a shortened URL with a magnet link and then send it to the user.
 
-# To-Do
-1. Specific message, when there's no search results.<br>
+# Changelog
+**2023/03/13** - Removed `Search` button, now bot expects that each message is a search query.<br>
--- bot.py
+++ bot.py
@@ -4,22 +4,15 @@ from enum import Enum
 
 from pydantic import BaseModel
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import Bot, Dispatcher, executor, types
 from decouple import config
 
 from api_handler import search, get_magnet
 
 storage = MemoryStorage()
-bot = Bot(token=config('TOKEN'))
+bot = Bot(token=config("TOKEN"))
 dp = Dispatcher(bot=bot, storage=storage)
-
-
-class Form(StatesGroup):
-    search = State()
 
 
 class MessageModel(BaseModel):
@@ -34,7 +27,7 @@ class Constants(Enum):
     absolute_path = os.path.dirname(__file__)
     MESSAGE_FILE = os.path.join(absolute_path, "templates/messages.json")
     torrentId = "torrentId"
-    magnetLink = 'Magnet link'
+    magnetLink = "Magnet link"
 
 
 MESSAGES = MessageModel.parse_file(Constants.MESSAGE_FILE.value)
@@ -46,8 +39,9 @@ def search_keyboard(search_results: list):
     for line in search_results:
         button_text = MESSAGES.search_button_template.format(*line.values())
         callback_data = line.get(Constants.torrentId.value)
-        line_button = InlineKeyboardButton(text=button_text,
-                                           callback_data=callback_data)
+        line_button = InlineKeyboardButton(
+            text=button_text, callback_data=callback_data
+        )
         search_keyboard.add(line_button)
     return search_keyboard
 
@@ -61,20 +55,15 @@ def magnet_keyboard(magnetLink: str):
     return magnet_keyboard
 
 
-def main_keyboard():
-    """Creates main menu buttons."""
-    search_button = KeyboardButton('Search')
-    mainmenu = ReplyKeyboardMarkup(resize_keyboard=True).add(search_button)
-    return mainmenu
-
-
-@dp.message_handler(state=None, commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
     """Handles the /start command. Welcomes the user and generates main menu
     keyboard."""
-    await bot.send_message(message.from_user.id, MESSAGES.start.format(
-        message.from_user.first_name), reply_markup=main_keyboard(),
-                           parse_mode='MarkdownV2')
+    await bot.send_message(
+        message.from_user.id,
+        MESSAGES.start.format(message.from_user.first_name),
+        parse_mode="MarkdownV2",
+    )
 
 
 @dp.callback_query_handler(lambda callback_query: True)
@@ -83,30 +72,26 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     button with magnet link."""
     torrentId = callback_query.data
     magnetLink = get_magnet(torrentId)
-    await bot.send_message(callback_query.from_user.id, MESSAGES.link,
-                           reply_markup=magnet_keyboard(magnetLink),
-                           parse_mode="MarkdownV2")
+    await bot.send_message(
+        callback_query.from_user.id,
+        MESSAGES.link,
+        reply_markup=magnet_keyboard(magnetLink),
+        parse_mode="MarkdownV2",
+    )
 
 
-@dp.message_handler(state=None, text="Search")
-async def search_handler(message: types.Message):
-    """Handles the Search button in main menu. Starts searching proccess and
-    creates the State for the bot."""
-    await bot.send_message(message.from_user.id,
-                           MESSAGES.search, parse_mode="MarkdownV2")
-    await Form.search.set()
-
-
-@dp.message_handler(state=Form.search)
-async def results_handler(message: types.Message, state: FSMContext):
+@dp.message_handler()
+async def results_handler(message: types.Message):
     """Finishes the Search state for the bot and sends results as inline
     buttons."""
     search_query = message.text
     search_results = search(search_query)
-    await bot.send_message(message.from_user.id, MESSAGES.results,
-                           reply_markup=search_keyboard(search_results),
-                           parse_mode="MarkdownV2")
-    await state.finish()
+    await bot.send_message(
+        message.from_user.id,
+        MESSAGES.results,
+        reply_markup=search_keyboard(search_results),
+        parse_mode="MarkdownV2",
+    )
 
 
 if __name__ == "__main__":
